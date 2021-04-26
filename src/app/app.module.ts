518f9dd7b8e36ce3209dbd4d21349a7a14c69cad
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-04-26 13:41:23 -0400

    routing and header set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { DogListComponent } from './dog-list/dog-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FavoriteListComponent } from './favorite-list/favorite-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DogListComponent,
     FavoriteListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
